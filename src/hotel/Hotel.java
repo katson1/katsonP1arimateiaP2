2b6275ef5d330b87f2b88a1a8b95f4bf70169de2
Katson Matheus <katson.alves@ccc.ufcg.edu.br>
2017-06-29 11:43:02 -0300

    Criei o classe Hotel

---
+++ src/hotel/Hotel.java
@@ -0,0 +1,41 @@
+package hotel;
+
+import java.util.ArrayList;
+
+public class Hotel {
+	
+	private Recepcao recepcao = new Recepcao();
+	
+	public Hotel(){
+		
+	}
+	
+	public void checkIn(String nome, String tipo, int idade, int dias, double valor){
+		recepcao.checkIn(nome, tipo, idade, dias, valor);
+		
+	}
+	
+	public void checkOut(String nome){
+			recepcao.checkOut(nome);
+			
+	}
+	
+	public int getNumDeHospedes(){
+		return recepcao.getNumDeHospedes();
+		
+	}
+	
+	public double getLucroTotal(){
+		return recepcao.getLucroTotal();
+	}
+	
+	public String toString(){
+		return recepcao.toString();
+		
+	}
+	
+}
+	
+
+	
+
